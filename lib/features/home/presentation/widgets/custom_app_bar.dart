40edb4fd23ca5517a27cb9f0ca22361c20ee40b5
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2023-11-08 14:04:16 +0200

    create custom app bar

---
+++ lib/features/home/presentation/widgets/custom_app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:book_ly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            AppAssets.logo,
+            height: 18,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search_outlined,
+              size: 30,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
