6be7ef002c1d52a00a458f9e70d815366967c11b
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2023-11-09 15:40:13 +0200

    Best seller list view item part one

--- lib/features/home/presentation/widgets/home_view_body.dart
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:book_ly/core/utils/assets.dart';
 import 'package:book_ly/core/utils/styles.dart';
 import 'package:book_ly/features/home/presentation/widgets/custom_app_bar.dart';
 import 'package:book_ly/features/home/presentation/widgets/featured_books_list_view.dart';
@@ -19,6 +20,37 @@ class HomeViewBody extends StatelessWidget {
           Text(
             "Best Seller",
             style: AppStyles.titleMeduim,
+          ),
+          SizedBox(height: 20),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    AppAssets.testImage,
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
