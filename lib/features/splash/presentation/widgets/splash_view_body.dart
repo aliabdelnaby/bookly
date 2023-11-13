8b024cf673cf89e2fb4304fff79d09147cc289fc
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2023-11-13 13:18:48 +0200

    refactor and add go router package

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,9 +1,8 @@
-import 'package:book_ly/constants.dart';
+import 'package:book_ly/core/utils/app_router.dart';
 import 'package:book_ly/core/utils/assets.dart';
-import 'package:book_ly/features/home/presentation/views/home_view.dart';
 import 'package:book_ly/features/splash/presentation/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -56,11 +55,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   void navigateToHomeView() {
     Future.delayed(const Duration(seconds: 2), () {
-      Get.to(
-        () => const HomeView(),
-        transition: Transition.fade,
-        duration: kTransitionDuration,
-      );
+      GoRouter.of(context).push(AppRouter.kHomeView);
     });
   }
 }
