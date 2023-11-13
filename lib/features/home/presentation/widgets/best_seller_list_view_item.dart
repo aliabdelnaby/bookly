9cd6dd7cefaf5d144be08530f6b949ab03b3feaf
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2023-11-13 13:34:55 +0200

    Create custom book details appbar

--- lib/features/home/presentation/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/widgets/best_seller_list_view_item.dart
@@ -1,70 +1,77 @@
 import 'package:book_ly/constants.dart';
+import 'package:book_ly/core/utils/app_router.dart';
 import 'package:book_ly/core/utils/assets.dart';
 import 'package:book_ly/core/utils/styles.dart';
 import 'package:book_ly/features/home/presentation/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    AppAssets.testImage,
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kBookDetailsView);
+      },
+      child: SizedBox(
+        height: 125,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.5 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      AppAssets.testImage,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(width: 30),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .5,
-                  child: Text(
-                    "Harry Potter And the Golbet of Fire",
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style: AppStyles.textStyle20.copyWith(
-                      fontFamily: kGTSectraFine,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 3),
-                const Text(
-                  "J.K Rowling",
-                  style: AppStyles.textStyle14,
-                ),
-                const SizedBox(height: 3),
-                Row(
-                  children: [
-                    Text(
-                      "19.99 €",
+            const SizedBox(width: 30),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .5,
+                    child: Text(
+                      "Harry Potter And the Golbet of Fire",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
                       style: AppStyles.textStyle20.copyWith(
-                        fontWeight: FontWeight.bold,
+                        fontFamily: kGTSectraFine,
                       ),
                     ),
-                    const Spacer(),
-                    const BookRating(),
-                  ],
-                ),
-              ],
+                  ),
+                  const SizedBox(height: 3),
+                  const Text(
+                    "J.K Rowling",
+                    style: AppStyles.textStyle14,
+                  ),
+                  const SizedBox(height: 3),
+                  Row(
+                    children: [
+                      Text(
+                        "19.99 €",
+                        style: AppStyles.textStyle20.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Spacer(),
+                      const BookRating(),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
