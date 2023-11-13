eb294adadb708af3189a1a6646bcfc3f8e6b7dd9
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2023-11-13 13:00:40 +0200

    Create book details view

---
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -0,0 +1,13 @@
+import 'package:book_ly/features/home/presentation/widgets/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}
