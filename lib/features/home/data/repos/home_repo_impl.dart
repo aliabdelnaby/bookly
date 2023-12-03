dc1ecfe25bc07b37485b202e1a6f7ab3b512409e
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2023-12-03 14:12:49 +0200

    Implement fetch featured books method

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -31,8 +31,22 @@ class HomeRepoImp implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async{
+  try {
+      var data = await apiService.get(
+          endPoint:
+              "volumes?filtering=free-ebooks&q=subject:programming");
+
+      List<BookModel> books = [];
+      for (var item in data["items"]) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.dioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
