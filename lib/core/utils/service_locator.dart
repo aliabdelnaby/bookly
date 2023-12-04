ff2698c554238ae2dd5cc4a58ea84708d493eaae
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2023-12-04 15:37:16 +0200

    Add get it & Setup service locator

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,11 @@
+import 'package:book_ly/core/utils/api_service.dart';
+import 'package:book_ly/features/home/data/repos/home_repo_impl.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImp>(HomeRepoImp(getIt.get<ApiService>()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:book_ly/core/utils/service_locator.dart';
+import 'package:book_ly/features/home/data/repos/home_repo_impl.dart';
+import 'package:book_ly/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:book_ly/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'constants.dart';
 import 'core/utils/app_router.dart';
 import 'package:flutter/material.dart';
@@ -12,12 +17,23 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImp>()),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(getIt.get<HomeRepoImp>()),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
